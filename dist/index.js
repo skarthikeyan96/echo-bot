d4327c6cc86d8aebeed22716762a2f2f93e00a59
Karthikeyan <imkarthikeyans@gmail.com>
2024-06-19 21:44:09 +0530

    working fine

--- dist/index.js
+++ dist/index.js
@@ -6,20 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // src/index.js
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const twilio_1 = __importDefault(require("twilio"));
+const twilio_1 = require("twilio");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
-const accountSid = process.env.ACCOUNTID;
-const authToken = process.env.AUTHTOKEN;
-// const client = new twilio(accountSid, authToken);
+app.use(express_1.default.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
 });
-const MessagingResponse = twilio_1.default.twiml.MessagingResponse;
+const MessagingResponse = twilio_1.twiml.MessagingResponse;
 app.post('/incoming', (req, res) => {
     const message = req.body;
-    console.log(`Received message from ${message.From}: ${message.Body}`);
+    console.log(req);
+    console.log(`Received message from ${message.Body}`);
     const twiml = new MessagingResponse();
     twiml.message(`You said: ${message.Body}`);
     res.writeHead(200, { 'Content-Type': 'text/xml' });
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,31 @@
 // src/index.js
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import twilio from 'twilio';
+import {twiml} from 'twilio';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
-const accountSid = process.env.ACCOUNTID;
-const authToken = process.env.AUTHTOKEN;
-// const client = new twilio(accountSid, authToken);
+
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-const MessagingResponse = twilio.twiml.MessagingResponse;
+const MessagingResponse = twiml.MessagingResponse;
 
 app.post('/incoming', (req, res) => {
   const message = req.body;
-  console.log(`Received message from ${message.From}: ${message.Body}`);
+  console.log(req)
+  console.log(`Received message from ${message.Body}`);
   const twiml = new MessagingResponse();
   twiml.message(`You said: ${message.Body}`);
   res.writeHead(200, { 'Content-Type': 'text/xml' });
   res.end(twiml.toString());
+  
+
 });
 
 app.listen(port, () => {
